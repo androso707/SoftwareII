df524db6a5f91cb866a94e4a41799431a1827243
linaarias95 <38679065+linaarias95@users.noreply.github.com>
2019-03-27 09:20:58 -0500

    lina

--- Dominio/tramitador.cs
+++ Dominio/tramitador.cs
@@ -4,11 +4,22 @@ using System.Text;
 
 namespace Dominio
 {
-    class tramitador : usuario
+    class Tramitador : usuario
     {
         object pasado_judicial;
         string descripcion;
         object experiencia;
         string tipo_vehiculo;
+
+      
+
+        public Tramitador(object pasado_judicial, string descripcion, object experiencia, string tipo_vehiculo,int cedula, string nombre, string privilegios, int telefono, string email, string direccion, string contraseña, DateTime fecha_nacimiento) : base(cedula, nombre, privilegios, telefono, email, direccion, contraseña, fecha_nacimiento)
+        {
+            this.pasado_judicial = pasado_judicial;
+            this.descripcion = descripcion;
+            this.experiencia = experiencia;
+            this.tipo_vehiculo = tipo_vehiculo;
+        }
+    }
     }
 }
--- Dominio/tramite.cs
+++ Dominio/tramite.cs
@@ -27,7 +27,7 @@ namespace Dominio
         private string v9;
         private double v10;
 
-        public tramite(int id_tramite, string estado, string nombre, string tipo, DateTime fecha, float tiempo, string descropcion, string lugar_origen, string lugar_destino, string transporte, double conto_total)
+        public tramite(int id_tramite, string estado, string nombre, string tipo, DateTime fecha, float tiempo, string descropcion, string lugar_origen, string lugar_destino, string transporte, double conto_total, int v1, string v2, string v3, string v4, DateTime dateTime, float v5, string v6, string v7, string v8, string v9, double v10)
         {
             this.id_tramite = id_tramite;
             this.estado = estado;
@@ -40,32 +40,17 @@ namespace Dominio
             this.lugar_destino = lugar_destino;
             this.transporte = transporte;
             this.conto_total = conto_total;
-        }
-
-        public tramite(int v1, int id_tramite, string v2, string estado, string v3, string nombre, string v4, string tipo, DateTime dateTime, DateTime fecha, float v5, float tiempo, string v6, string descropcion, string v7, string lugar_origen, string v8, string lugar_destino, string v9, string transporte, double v10, double conto_total)
-        {
             this.v1 = v1;
-            this.id_tramite = id_tramite;
             this.v2 = v2;
-            this.estado = estado;
             this.v3 = v3;
-            this.nombre = nombre;
             this.v4 = v4;
-            this.tipo = tipo;
             this.dateTime = dateTime;
-            this.fecha = fecha;
             this.v5 = v5;
-            this.tiempo = tiempo;
             this.v6 = v6;
-            this.descropcion = descropcion;
             this.v7 = v7;
-            this.lugar_origen = lugar_origen;
             this.v8 = v8;
-            this.lugar_destino = lugar_destino;
             this.v9 = v9;
-            this.transporte = transporte;
             this.v10 = v10;
-            this.conto_total = conto_total;
         }
     }
 
