781541d711988781fb239fb8cd29023af88f0a56
oscar <oscarivanbp@gmail.com>
2019-03-27 09:41:01 -0500

    Oscar

--- Dominio/administrador.cs
+++ Dominio/administrador.cs
@@ -6,5 +6,8 @@ namespace Dominio
 {
     class administrador : usuario
     {
+        public Administrador(int cedula, string nombre, string privilegios, int telefono, string email, string direccion, string contraseña, DateTime fecha_nacimiento) : base(cedula, nombre, privilegios, telefono, email, direccion, contraseña, fecha_nacimiento)
+        {
+        }
     }
 }
--- Dominio/usuario.cs
+++ Dominio/usuario.cs
@@ -14,17 +14,6 @@ namespace Dominio
         string contraseña;
         DateTime fecha_nacimiento;
 
-        public usuario(int cedula, string nombre, string privilegios, int telefono, string email, string direccion, string contraseña, DateTime fecha_nacimiento)
-        {
-            this.cedula = cedula;
-            this.nombre = nombre;
-            this.privilegios = privilegios;
-            this.telefono = telefono;
-            this.email = email;
-            this.direccion = direccion;
-            this.contraseña = contraseña;
-            this.fecha_nacimiento = fecha_nacimiento;
-        }
     }
 
 }
