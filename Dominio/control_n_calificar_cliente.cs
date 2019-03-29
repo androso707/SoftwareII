d3c5537ee3508b973822c6dc19b4fc8c960aceb0
oscar <oscarivanbp@gmail.com>
2019-03-29 15:32:07 -0500

    Oscar

--- Dominio/control_n_calificar_cliente.cs
+++ Dominio/control_n_calificar_cliente.cs
@@ -6,5 +6,45 @@ namespace Dominio
 {
     class control_n_calificar_cliente
     {
+
+'# Existe_cliente(idcliente)!=false,
+'# consultar_estado_tramite(idtramite)==”TERMINADO”,
+'# String.IsNullOrEmpty(nombre),
+'# String.IsNullOrEmpty(tipo),
+'# String.IsNullOrEmpty(calificacion),
+'//fdfjdfd
+
+
+
+
+        public bool Existe_cliente(String idTramite)
+        {
+            
+
+            return true;
+        }
+
+
+        public String Consultar_estado_tramite(String idtramite)
+        {
+
+            return idtramite;
+        }
+
+        public void Guardar_calificacion(String nombre,DateTime fecha,String tipo,String descripcion,String estado,String calificacion,String comentarios)
+        {
+            Console.WriteLine("Calificacion Exitoda del Cliente");
+        }
+
+
+
+
+        public static void Main(String[] args)
+        {
+
+            Console.WriteLine("Calificar Cliente prueba");
+        }
+
+
     }
 }
