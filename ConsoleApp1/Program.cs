e7e059984bcccf393eb0e2b230a31afe8725c993
linaarias95 <38679065+linaarias95@users.noreply.github.com>
2019-03-30 14:54:10 -0500

    lina

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,6 +15,10 @@ namespace EnsayoOscar
             control_n_calificar_cliente metodos = new control_n_calificar_cliente();
             metodos.Existe_cliente("jashs");
             Console.WriteLine(metodos.Existe_cliente("jashs"));
+            control_n_registrar_tramitador registrarT = new control_n_registrar_tramitador();
+            DateTime thisDate1 = new DateTime(1900, 6, 10);
+            int valor1=5;
+            registrarT.comprobar_campos(valor1,"jose","ds","tramitadora",3145555,"paice123@gmail.com","calle 34","paicemiau", thisDate1,"","","","carro");
             Console.ReadKey();
         }
     }
