0b1318ba63d834d821c6125d27af9e3e6a4de765
linaarias95 <38679065+linaarias95@users.noreply.github.com>
2019-03-31 14:53:31 -0500

    lina

--- ConsoleApp1/conexion.cs
+++ ConsoleApp1/conexion.cs
@@ -4,11 +4,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
 
-Impports sys
 namespace ConsoleApp1
 {
     class conexion
     {
+        SqlConnection cn;
+        SqlCommand cmd;
+        SqlDataReader crd;
+
+        public conexion() {
+            try
+            {
+            }
+            catch (Exception ex) {
+            }
+        }
+
+        }
     }
 }
