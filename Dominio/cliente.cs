a99578276263382f0dee41aae4a8b8a1887d756a
linaarias95 <38679065+linaarias95@users.noreply.github.com>
2019-03-31 16:16:25 -0500

    lina

--- Dominio/cliente.cs
+++ Dominio/cliente.cs
@@ -4,8 +4,11 @@ using System.Text;
 
 namespace Dominio
 {
-    class Cliente
+    class Cliente:usuario
     {
+        public Cliente(int cedula, string nombre, string privilegios, int telefono, string email, string direccion, string contraseña, DateTime fecha_nacimiento) : base(cedula, nombre, privilegios, telefono, email, direccion, contraseña, fecha_nacimiento) {
+        }
+
 
     }
 }
