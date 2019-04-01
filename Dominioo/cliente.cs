8b2a8cd163baca160fe7869110f441fe6f14baf4
felipe osorio <androso707@gmail.com>
2019-04-01 16:16:47 -0500

    Correccion atributos Heredados

--- Dominioo/cliente.cs
+++ Dominioo/cliente.cs
@@ -7,6 +7,8 @@ namespace Dominio
     class Cliente:usuario
     {
         public Cliente(int cedula, string nombre, string privilegios, int telefono, string email, string direccion, string contraseña, DateTime fecha_nacimiento) : base(cedula, nombre, privilegios, telefono, email, direccion, contraseña, fecha_nacimiento) {
+
+
         }
 
 
