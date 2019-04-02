db62d2150adfc55b5e072b4e373ee168e7f2c109
linaarias95 <38679065+linaarias95@users.noreply.github.com>
2019-04-01 19:17:16 -0500

    lina

--- Dominioo/Program.cs
+++ Dominioo/Program.cs
@@ -18,7 +18,7 @@ namespace Dominioo
             
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form2());
             
 
         }
