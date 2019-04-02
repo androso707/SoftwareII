292fc9d420c9daf2b82aec35a3170e9bdeccbdbc
Leonardo Arango <rocker1058@gmail.com>
2019-04-01 22:15:09 -0500

    Leo

--- Dominioo/ControlVistaOrdenar.cs
+++ Dominioo/ControlVistaOrdenar.cs
@@ -16,22 +16,26 @@ namespace Dominioo
         SqlConnection cone = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Leo\Documents\Database.mdf;Integrated Security=True;Connect Timeout=30");
         public ControlVistaOrdenar()
         {
-            
+
             InitializeComponent();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             cone.Open();
+            MessageBox.Show("se abrio");
             SqlCommand cmd = cone.CreateCommand();
-            SqlDataReader reader = cmd.ExecuteReader();
-            if (reader.Read())
-            {
-            }
-
-            cmd.CommandText = "INSERT [Tramite] (id_tramite,estado,nombre,tipo,fecha,transporte,tiempo,descripcion,lugarorigen,lugardestino) VALUES ('" + idtxt.Text + "','" + txtnombre.Text +"')";
-            MessageBox.Show("Tramite Isertado");
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select nombre from [Tramite] where id_tramite= ('" + idtxt.Text + "')";
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("encontrado");
+            cmd.CommandText = "INSERT [Tramite] (id_tramite,nombre) VALUES ('" + idtxt.Text + "','" + txtnombre.Text + "')";
+            cmd.ExecuteNonQuery();
             cone.Close();
+            MessageBox.Show("datos insertados");
+            
+           
+
         }
     }
 }
